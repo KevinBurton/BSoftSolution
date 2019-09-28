50c14f085f99e90bd190e5d2e107ad5161851102
Ronald Kevin Burton <rkevinburton@charter.net>
2019-09-28 18:49:48 -0500

    Added real resume

--- BSoftSolutions/ClientApp/src/app/resume/resume.component.ts
+++ BSoftSolutions/ClientApp/src/app/resume/resume.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-resume',
-  templateUrl: './resume.component.html',
+    templateUrl: './resume.component.html',
+    styleUrls: ['./resume.component.css']
 })
 export class ResumeComponent {
 }
