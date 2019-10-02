ce25a1472d25252efc23ec13b2359894269d94a1
Kevin Burton <rkevinburton@charter.net>
2019-10-02 08:41:37 -0500

    Use Angular dependency injector

--- BSoftSolutions/ClientApp/src/app/geoprocessing/geoprocessing.component.ts
+++ BSoftSolutions/ClientApp/src/app/geoprocessing/geoprocessing.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { GeoProcessor } from '../shared/geoprocessor.service';
 
 @Component({
     selector: 'app-geoprocessing',
     templateUrl: './geoprocessing.component.html'
 })
 export class GeoProcessingComponent {
+    constructor(private processor: GeoProcessor) {
+
+    }
     geocodeInput: string = '';
     processGeocodeInput(): void {
         console.log(`Geocode input: ${this.geocodeInput}`);
