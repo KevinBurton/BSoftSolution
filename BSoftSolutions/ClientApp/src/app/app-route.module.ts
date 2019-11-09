f1392bcc2fd0a058888d7dbbe5d2d9c847a5f9f7
Kevin Burton <rkevinburton@charter.net>
2019-11-09 13:07:15 -0600

    Added routine module

---
+++ BSoftSolutions/ClientApp/src/app/app-route.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
+
+import { HomeComponent } from './home/home.component';
+import { ResumeComponent } from './resume/resume.component';
+import { CounterComponent } from './counter/counter.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { GeoProcessingComponent } from './geoprocessing/geoprocessing.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+    { path: '', component: HomeComponent, pathMatch: 'full' },
+    { path: 'geo', component: GeoProcessingComponent },
+    { path: 'resume', component: ResumeComponent },
+    { path: 'counter', component: CounterComponent },
+    { path: 'fetch-data', component: FetchDataComponent },
+    { path: '**', component: PageNotFoundComponent }
+];
+
+
+@NgModule({
+  declarations: [
+        HomeComponent,
+        ResumeComponent,
+        CounterComponent,
+        FetchDataComponent,
+        GeoProcessingComponent,
+        PageNotFoundComponent
+    ],
+  imports: [
+      CommonModule,
+      RouterModule.forRoot(routes)
+    ],
+  exports: [RouterModule]
+})
+export class AppRouteModule { }
